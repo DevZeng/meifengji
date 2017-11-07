116bd22021014130c88b7c2e62d4c39de75a8f7b
DevZeng <devzeng@sennki.com>
2017-11-07 17:13:39 +0800

    fix

--- app/Http/Controllers/API/V1/UserController.php
+++ app/Http/Controllers/API/V1/UserController.php
@@ -42,6 +42,7 @@ class UserController extends Controller
                     return response()->json([
                         'code'=>'200',
                         'data'=>[
+                            'worker'=>$user->worker,
                             'token'=>$token
                         ]
                     ]);
@@ -52,6 +53,7 @@ class UserController extends Controller
                 return response()->json([
                     'code'=>'200',
                     'data'=>[
+                        'worker'=>$user->worker,
                         'token'=>$token
                     ]
                 ]);
