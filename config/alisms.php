f8323b05285c4b3b96f49a69b35532f678fb673f
DevZeng <devzeng@sennki.com>
2017-11-08 12:04:20 +0800

    add sms

--- config/alisms.php
+++ config/alisms.php
@@ -8,7 +8,7 @@
 return [
     'KEY' =>'24686003',
     'SECRETKEY'=>'2a987b373fb7a3326fa7344949326032',
-    'company'=>'真瓷美缝剂',
+    'company'=>'美家美缝服务中心',
     'VerificationCode'=>'SMS_109420184',
     'Notify'=>'SMS_109345194',
     'Pass'=>'SMS_109565185'
