133b0c5e81e5b1288a1d80b98c5f244600284a56
DevZeng <devzeng@sennki.com>
2017-11-15 16:58:26 +0800

    fix

--- config/wxxcx.php
+++ config/wxxcx.php
@@ -9,6 +9,6 @@ return [
     'app_id'=>'wx367cd3e4c052b742',
     'app_secret'=>'d79df0e066d260f94f5ed6629d4106e3',
     'mch_id'=>'1424053202',
-    'notify_url'=>'http://119.23.255.177:8090/api/v1/pay/notify',
+    'notify_url'=>'http://fwq.gdmeika.com/api/v1/pay/notify',
     'api_key'=>'GongDuanMEIJIAKACOM4006020288888'
 ];
