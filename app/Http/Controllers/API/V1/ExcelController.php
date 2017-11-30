cb350292e7c9e356e3e4a2f79d6301610e9a66c0
DevZeng <devzeng@sennki.com>
2017-11-30 11:19:55 +0800

    fix

--- app/Http/Controllers/API/V1/ExcelController.php
+++ app/Http/Controllers/API/V1/ExcelController.php
@@ -71,10 +71,10 @@ class ExcelController extends Controller
                 break;
         }
         $final = array_merge($origin,$data);
-        return response()->json([
-            'code'=>200,
-            'data'=>$final
-        ]);
+//        return response()->json([
+//            'code'=>200,
+//            'data'=>$final
+//        ]);
         \Maatwebsite\Excel\Facades\Excel::create($name,function ($excel) use ($final){
             $excel->sheet('sheet1',function ($sheet) use ($final){
                 $count = count($final);
