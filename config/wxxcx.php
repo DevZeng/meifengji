de1ec91542f940af0158358184206c4903cb1108
DevZeng <zengshungeng@live.com>
2018-05-11 12:05:17 +0800

    Update wxxcx.php

--- config/wxxcx.php
+++ config/wxxcx.php
@@ -9,7 +9,7 @@ return [
     'app_id'=>'wx367cd3e4c052b742',
     'app_secret'=>'d79df0e066d260f94f5ed6629d4106e3',
     'mch_id'=>'1424053202',
-    'notify_url'=>'https://xcx.xiashantown.cn/api/v1/pay/notify',
+    'notify_url'=>'https://xcx.gdmeika.com/api/v1/pay/notify',
     'api_key'=>'GongDuanMEIJIAKACOM4006020288888',
     'template_id'=>'xDr8WW2-lNEt3bwzaIXc1R0Y0FzCTnQk1B5p4t9tQPc'
 ];
